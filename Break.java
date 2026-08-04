cb87e911e9b1970d4cda5bf2000ade8de6aa84f9
satyam maurya <satyammauryas368@gmail.com>
2026-08-04 21:23:15 +0530

    Day 5: Break Statement

---
+++ Break.java
@@ -0,0 +1,13 @@
+
+public class Break {
+
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 0; i <= 5; i++) {
+            if (i == 3) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
